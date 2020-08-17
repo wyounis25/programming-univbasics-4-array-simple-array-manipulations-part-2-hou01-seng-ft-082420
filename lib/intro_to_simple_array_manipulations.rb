e9fe4df7b1a2e247faab5580d033d67a1e445ec7
walid younis <walid.younis.90@gmail.com>
2020-08-17 22:49:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,3 +3,15 @@ def using_concat(array,element)
   array.concat(element)
   
 end 
+
+def using_insert(Array,element)
+  
+  array.concat(element)
+  
+end 
+
+def using_concat(array,element)
+  
+  array.concat(element)
+  
+end 
